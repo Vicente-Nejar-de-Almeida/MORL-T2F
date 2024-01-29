- Computation of the online normalization - Introduction of an early stop validation - Fixing code for the normalization and early stop validation

first_experiments.py
@@ -1,6 +1,7 @@
 # Application of Time2Feat on real dataset
 import pickle
-
+import csv
+import time
 import numpy as np
 import pandas as pd
 from t2f.extraction.extractor import feature_extraction
@@ -11,6 +12,15 @@ from t2f.selection.selection import cleaning
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import adjusted_mutual_info_score
 import os
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score
+from jqmcvi.base import dunn_fast
+import NormalizedScore as ns
+
+from scipy.stats import pearsonr
+from early_stopping_class import CustomEarlyStopping
 
 from rl.environments.feature_selection_env import FeatureSelectionEnvironment
 from rl.agents.ql_agent import QLAgent
@@ -19,14 +29,24 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    listNameDataset = ["RacketSports"]
+    listNameDataset = ["ERing"]
     transform_type = 'minmax'
     model_type = 'Hierarchical'
     train_size = 0.3
     batch_size = 500
-    p = 8
+    p = os.cpu_count()
+    f = open('resultsT2RL.csv', 'w+', newline='')
+    writer = csv.writer(f)
+    header = ["Dataset", "n_feats", "AMI", "Time"]
+    writer.writerow(header)
+
+    silhouette_norm = ns.NormalizedScore(silhouette_score)
+    calinski_norm = ns.NormalizedScore(calinski_harabasz_score)
+
+    norm_list = [silhouette_norm, calinski_norm]
 
     for nameDataset in listNameDataset:
+        extractTime = 0
         # Read original dataset
         # print('Read ucr datasets: ', files)
         ts_list, y_true = read_ucr_datasets(nameDataset=nameDataset)
@@ -45,10 +65,12 @@ if __name__ == '__main__':
             idx_train, _, y_train, _ = train_test_split(
                 np.arange(len(ts_list)), y_true, train_size=train_size)
             labels = {i: j for i, j in zip(idx_train, y_train)}
-            print('Number of Labels: {}'.format(len(labels)))
+            # print('Number of Labels: {}'.format(len(labels)))
 
         if not os.path.isfile("data//"+nameDataset+"_feats.pkl"):
+            timeStart = time.time()
             df_all_feats = feature_extraction(ts_list, batch_size, p)
+            extractTime = time.time() - timeStart
             file = open("data//"+nameDataset+"_feats.pkl", 'wb')
             pickle.dump(df_all_feats, file)
 
@@ -56,61 +78,74 @@ if __name__ == '__main__':
             df_all_feats = pickle.load(pickle_file)
 
         df_all_feats = cleaning(df_all_feats)
-        total_number_of_features = len(df_all_feats.columns)
-        print('Total number of features:', total_number_of_features)
+        print(len(df_all_feats))
+        total_number_of_features = [50]
+        episodes_total = [200]
 
-        episodes = 100
-        n_features = round(total_number_of_features * 0.15)
-        # n_features = 130
-        env = FeatureSelectionEnvironment(
-            df_features=df_all_feats,
-            n_features=n_features,
-            clustering_model=model,
-        )
 
-        obs, info = env.reset()
-        done = False
+        for episodes in episodes_total:
+            print('Total number of features:', total_number_of_features)
+            for n_features in total_number_of_features:
+                if n_features > len(df_all_feats.columns):
+                    n_features = len(df_all_feats.columns) - 1
+                startTime = time.time()
+                # n_features = 130
+                env = FeatureSelectionEnvironment(
+                    df_features=df_all_feats,
+                    n_features=n_features,
+                    clustering_model=model,
+                    list_eval=norm_list
+                )
 
-        agent = QLAgent(starting_state=tuple(
-            obs), state_space=env.observation_space, action_space=env.action_space)
+                obs, info = env.reset(norm_list)
+                done = False
 
-        results = {e: {
-            'reward': [],
-            'total_score': [],
-            'dbs': [],
-            'scoreDBS': [],
-            'chs': [],
-            'scoreCHS': [],
-            'dus': [],
-            'scoreDuS': [],
-            'sils': [],
-            'scoreSilS': [],
-        } for e in range(episodes)}
+                agent = QLAgent(starting_state=tuple(
+                    obs), state_space=env.observation_space, action_space=env.action_space)
 
-        AMIVal = []
-        y_pred = y_true
-        for episode in tqdm(range(episodes)):
-            # print(f'Episode {episode}')
-            while not done:
-                action = agent.act(info['legal_actions'])
-                # print('Action:', action)
-                next_obs, reward, terminated, truncated, info, y_pred = env.step(action)
-                AMIVal.append(adjusted_mutual_info_score(y_pred, y_true))
-                # print("AMI-Inside: ",adjusted_mutual_info_score(y_pred, y_true))
-                results[episode]['reward'].append(reward)  # save reward
+                results = {e: {
+                    'reward': [],
+                    'total_score': [],
+                    'dbs': [],
+                    'scoreDBS': [],
+                    'chs': [],
+                    'scoreCHS': [],
+                    'dus': [],
+                    'scoreDuS': [],
+                    'sils': [],
+                    'scoreSilS': [],
+                } for e in range(episodes)}
 
-                agent.learn(tuple(next_obs), reward, done)
-                done = terminated or truncated
+                results_total = {val: {'total_score':[],'AMI':[]} for val in range(episodes)}
 
-            print("AMI: ", AMIVal[len(AMIVal)-1])
-            print('Variance: ', np.var(AMIVal))
-            for detailed_scores in info['score_history']:
-                for k, v in detailed_scores.items():
-                    results[episode][k].append(v)
+                AMIVal = []
+                y_pred = y_true
 
-            obs, info = env.reset()
-            done = False
+                for episode in tqdm(range(episodes)):
+                    # print(f'Episode {episode}')
+                    while not done:
+                        action = agent.act(info['legal_actions'])
+                        # print('Action:', action)
+                        next_obs, reward, terminated, truncated, info, features_selected = env.step(action)
+                        agent.learn(tuple(next_obs), reward, done)
+                        if len(features_selected) > n_features:
+                            done = True
+                        else:
+                            done = terminated or truncated
 
-        for episode in range(episodes):
-            results_df = pd.DataFrame(results[episode])
-            results_df.to_csv(f'results/results_episode{episode}.csv')
+
+                    for detailed_scores in info['score_history']:
+                        for k, v in detailed_scores.items():
+                            results[episode][k].append(v)
+
+                    obs, info = env.reset(norm_list)
+                    done = False
+                y_pred = model.fit_predict(df_all_feats[features_selected])
+                AMI = adjusted_mutual_info_score(y_pred, y_true)
+                finTime = (time.time() - startTime) + extractTime
+                results = [nameDataset, n_features, AMI, finTime]
+                writer.writerow(results)
+                print(f"{nameDataset}, has obtained a value of AMI equal to {AMI} with {len(env.best_conf_feat)} features with time {finTime}")
+                # print("The Dataset %s has obtained ")
+                # print("AMI: ", AMIVal[len(AMIVal) - 1])
+                print('**********************')

rl/environments/feature_selection_env.py
@@ -1,22 +1,25 @@
+import math
 import sys
 import gym
 import numpy as np
 from sklearn.metrics import davies_bouldin_score, calinski_harabasz_score, silhouette_score
 from jqmcvi.base import dunn_fast
-
+from early_stopping_class import CustomEarlyStopping
+from OnlineDeviationMean import OnlineDeviationMean
 
 def normalized_DBS(df_feat_all, y_pred):
-    upper_limit = 1000
+    upper_limit = 100
     dbs = davies_bouldin_score(df_feat_all, y_pred)
+
     if dbs > upper_limit:
         dbs = upper_limit
     score = ((dbs - upper_limit)/(0-upper_limit))
     # print('DBS:', str(dbs), '| Normalized:', str(score))
-    return dbs, score
+    return -dbs, score
 
 
 def normalized_CHS(df_feat_all, y_pred):
-    upper_limit = 1000
+    upper_limit = 100
     chs = calinski_harabasz_score(df_feat_all, y_pred)
     if chs > upper_limit:
         chs = upper_limit
@@ -26,7 +29,7 @@ def normalized_CHS(df_feat_all, y_pred):
 
 
 def normalized_DuS(df_feat_all, y_pred):
-    upper_limit = 1000
+    upper_limit = 100
     dus = dunn_fast(df_feat_all, y_pred)
     if dus > upper_limit:
         dus = upper_limit
@@ -42,40 +45,62 @@ def normalized_SilS(df_feat_all, y_pred):
     return sils, score
 
 
-def obtain_score(df_feat_all, y_pred):
+def obtain_score(df_feat_all, y_pred, list_eval):
+
+    '''
     dbs, scoreDBS = normalized_DBS(df_feat_all, y_pred)
     chs, scoreCHS = normalized_CHS(df_feat_all, y_pred)
     dus, scoreDuS = normalized_DuS(df_feat_all, y_pred)
     sils, scoreSilS = normalized_SilS(df_feat_all, y_pred)
-    scores = [scoreDBS, scoreCHS, scoreDuS, scoreSilS]
-    score = np.mean(scores)
-    return score, {
-        'total_score': score,
-        'dbs': dbs,
-        'scoreDBS': scoreDBS,
-        'chs': chs,
-        'scoreCHS': scoreCHS,
-        'dus': dus,
-        'scoreDuS': scoreDuS,
-        'sils': sils,
-        'scoreSilS': scoreSilS,
-    }
+
+
+    scoreTotal = []
+    for i in range(len(list_eval)):
+        if i == 0:
+            scoreTotal.append(list_eval[i].compute_normalize(dbs))
+        elif i == 1:
+            scoreTotal.append(list_eval[i].compute_normalize(chs))
+        elif i == 2:
+            scoreTotal.append(list_eval[i].compute_normalize(dus))
+        elif i == 3:
+            scoreTotal.append(list_eval[i].compute_normalize(sils))
+    for i in range(len(list_eval)):
+        if i == 0:
+            list_eval[0].add_data_point(dbs)
+        elif i == 1:
+            list_eval[1].add_data_point(chs)
+        elif i == 2:
+            list_eval[2].add_data_point(dus)
+        elif i == 3:
+            list_eval[3].add_data_point(sils)
+    '''
+
+    scoreTotal = []
+    for norm_score in list_eval:
+        scoreTotal.append(norm_score.get_normalized_value(df_feat_all, y_pred)[1])
+    if math.inf in scoreTotal or -math.inf in scoreTotal:
+        raise ValueError
+    else:
+        score = np.mean(scoreTotal)
+        # print("ScoreObtained: ", str(score))
+        return score
 
 
 class FeatureSelectionEnvironment(gym.Env):
 
-    def __init__(self, df_features, n_features, clustering_model) -> None:
+    def __init__(self, df_features, n_features, clustering_model, list_eval) -> None:
         self.observation_space = gym.spaces.Box(0, 1, shape=(len(df_features.columns),), dtype=np.float32)
         self.action_space = gym.spaces.Discrete(len(df_features.columns))
-        
         self.all_features = df_features.copy()
         self.current_state = np.zeros(len(self.all_features.columns))
         self.n_features = n_features
         self.clustering_model = clustering_model
-        
+        self.custom_early_stopping = CustomEarlyStopping(patience=20, plateau_patience=20)
         self.past_reward = 0
         self.score_history = []
-
+        self.best_conf_feat = []
+        self.sum_score = []
+        self.list_eval = list_eval
     
     def _get_obs(self) -> np.array:
         return self.current_state
@@ -86,14 +111,15 @@ class FeatureSelectionEnvironment(gym.Env):
             'score_history': self.score_history,
         }
     
-    def reset(self, seed=None, options=None):
-        
+    def reset(self, list_eval, seed=None, options=None):
         self.past_reward = 0
         self.score_history = []
-
+        self.custom_early_stopping = CustomEarlyStopping(patience=20, plateau_patience=20)
         self.current_state = np.zeros(len(self.all_features.columns))
         observation = self._get_obs()
         info = self._get_info()
+        self.sum_score = []
+        self.list_eval = list_eval
         return observation, info
     
     def _get_reward(self, action):
@@ -105,41 +131,47 @@ class FeatureSelectionEnvironment(gym.Env):
             (1) Use only the features selected by RL to compute metrics
             (2) Use raw time series to compute metrics
             """
-            score, detailed_scores = obtain_score(self.all_features, y_pred)
+            score = obtain_score(self.all_features, y_pred, self.list_eval)
+            self.sum_score.append(score)
         except ValueError:
             # happens when all labels have same value, thus no real "clustering" has occurred
             score = -1
-            detailed_scores = {
-                'total_score': np.nan,
-                'dbs': np.nan,
-                'scoreDBS': np.nan,
-                'chs': np.nan,
-                'scoreCHS': np.nan,
-                'dus': np.nan,
-                'scoreDuS': np.nan,
-                'sils': np.nan,
-                'scoreSilS': np.nan,
-            }
-        
+
         gain = score - self.past_reward
         self.past_reward = score
 
-        self.score_history.append(detailed_scores)
-        
-        return gain, y_pred
+        if len(self.sum_score) == 0:
+            sum_score_ins = None
+        else:
+            sum_score_ins = np.sum(self.sum_score)
+        return gain, y_pred, score, selected_features, sum_score_ins
     
     def step(self, action):
         self.current_state[action] = 1
-        reward, features = self._get_reward(action)
+        reward, features, score, features_selected, sum_score = self._get_reward(action)
         observation = self._get_obs()
         info = self._get_info()
+        if sum_score is not None:
+            checkContinuation = self.custom_early_stopping.update_metric(sum_score)
+            if checkContinuation == "Improve":
+                self.best_conf_feat = features_selected
+                terminated = False
+            elif checkContinuation == "Safe":
+                terminated = False
+            else:
+                terminated = True
+        else:
+            terminated = False
+
+
+        '''
         # Questo non va bene perché si deve fermare quando le performance sono stabili.
         if len(self.current_state[self.current_state == 1]) < self.n_features:
             terminated = False
         else:
             terminated = True
-
-        return observation, reward, terminated, False, info, features
+        '''
+        return observation, reward, terminated, False, info, features_selected
     
     def render(self):
         pass